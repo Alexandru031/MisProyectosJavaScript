65f718c7e3e3931e4c0673fda90bcb78575bae1a
Alexandru031 <alexandrustanica33@gmail.com>
2023-09-21 12:42:56 +0200

    Añade js ejer_1 y 2

--- JavaScript/Ejercicios_Practica/Ejercicio2/opcion2/nota.js
+++ JavaScript/Ejercicios_Practica/Ejercicio2/opcion2/nota.js
@@ -3,24 +3,32 @@ En el siguiente ejercicio vamos a practicar con el bucle do while para que el c�
 
 Pide una nota (número). Muestra la calificación según la nota:
  */
-function califica() {
-  let nota = parseInt(document.getElementById("nota").value);
-  /*  "document.getElementById("nota").value" - Sirve para obtener valor de cualquier tipo de dato
-      "parseInt()" - Convertir el valor en un Int */
-  
-  let texto;
-  if (nota >= 0 && nota < 3) {
-    texto = "Muy deficiente";
-  } else if (nota >= 3 && nota < 5) {
-    texto = "Insuficiente";
-  } else if (nota >= 5 && nota < 6) {
-    texto = "Suficiente";
-  } else if (nota >= 6 && nota < 7) {
-    texto = "Bien";
-  } else if (nota >= 7 && nota < 9) {
-    texto = "Notable";
-  } else if (nota >= 9 && nota <= 10){
-    texto = "Sobresaliente";
+
+let otraNota = true;
+do {
+  let nota = parseInt(prompt("Que nota tienes?"));
+
+  let noEsNota = !Number.isNaN(nota);
+
+  function califica() {
+    if (noEsNota) {
+      if (nota >= 0 && nota < 3) {
+        return "Muy deficiente";
+      } else if (nota >= 3 && nota < 5) {
+        return "Insuficiente";
+      } else if (nota >= 5 && nota < 6) {
+        return "Suficiente";
+      } else if (nota >= 6 && nota < 7) {
+        return "Bien";
+      } else if (nota >= 7 && nota < 9) {
+        return "Notable";
+      } else if (nota >= 9 && nota <= 10) {
+        return "Sobresaliente";
+      } 
+    } else {
+      return 'No es numero'
+    }
   }
-  document.getElementById("result").value = texto /* Coger el texto y mostrar en un input de la salida */
-}
+    document.write(noEsNota ? nota + " -> " + califica() + `<br>` : `No es numero<br>`)
+    otraNota = confirm("Quieres añadir más notas?");
+} while (otraNota);
